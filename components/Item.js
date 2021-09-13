e05fd31cf323ee70dbbaf5130a5ced932e7704f3
hafezfhmi <hafezfahmi2000@gmail.com>
2021-09-13 19:56:24 +0800

    Done adding searching funcionality to display data on rarity page

--- components/Item.js
+++ components/Item.js
@@ -2,19 +2,17 @@ import styles from '../styles/Item.module.scss';
 import utilStyles from '../styles/Utils.module.scss';
 import Image from 'next/image';
 
-function Item() {
+function Item({ NFT }) {
   return (
     <section className={styles.Item + ' ' + utilStyles.margin2Bot}>
-      <Image src="/images/Cat1.jpg" height="300" width="300" />
+      <Image src={`/images/NFT-med/${NFT.id}.jpg`} height="300" width="300" />
       <div className={styles.container}>
-        <h1>Rarity: 42 of 500</h1>
+        <h1>Rarity: {NFT.rank} of 500</h1>
+        <h2>ID: {NFT.id}</h2>
         <ul>
-          <li>Straw hats of 28</li>
-          <li>Straw hats of 28</li>
-          <li>Straw hats of 28</li>
-          <li>Straw hats of 28</li>
-          <li>Straw hats of 28</li>
-          <li>Straw hats of 28</li>
+          {NFT.attributes.map((curr) => (
+            <li key={curr}>&#9656; {curr}</li>
+          ))}
         </ul>
       </div>
     </section>
--- pages/rarity/[id].js
+++ pages/rarity/[id].js
@@ -1,11 +1,40 @@
 import Item from '../../components/Item';
 import Searchbar from '../../components/Searchbar';
 
-function Details() {
+// https://nextjs.org/docs/basic-features/data-fetching
+// https://www.youtube.com/watch?v=2zRHlqc0_yw
+// Make dynamic path for routing based on data using getStaticPaths and get the data using getStaticProps
+export const getStaticPaths = async () => {
+  const res = await fetch('http://localhost:3000/api/nftData');
+  const data = await res.json();
+
+  const paths = data.map((curr) => {
+    return {
+      params: { id: curr.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`http://localhost:3000/api/nftData/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { NFT: data },
+  };
+};
+
+function Details({ NFT }) {
   return (
     <>
       <Searchbar />
-      <Item />
+      <Item NFT={NFT} />
     </>
   );
 }
