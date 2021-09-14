30a6e3a2239e06c9ffd1c4a2f0c8852ff619c09f
hafezfhmi <hafezfahmi2000@gmail.com>
2021-09-15 02:30:26 +0800

    Made index responsive

---
+++ components/AttributeTable.js
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/AttributeTable.module.scss';
+import utilStyles from '../styles/Utils.module.scss';
+
+function AttributeTable({ data, sectionHeader, tableHeader }) {
+  return (
+    <section className={utilStyles.margin2TopBot}>
+      <h1 className={styles.sectionHeader}>{sectionHeader}</h1>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {tableHeader.map((curr) => (
+              <th
+                key={`${sectionHeader}${curr}`}
+                className={styles.tableHeader}
+              >
+                {curr}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((curr) => {
+            return (
+              <tr>
+                <td>
+                  <Link href={`http://localhost:3000/attribute/${curr.id}`}>
+                    <a>
+                      {sectionHeader == 'Attributes Amount'
+                        ? curr.id.match(/\d/g)
+                        : curr.id}
+                    </a>
+                  </Link>
+                </td>
+                <td>{curr.total}</td>
+                <td>
+                  {curr.accId.slice(0, 5).map((curr2) => {
+                    return (
+                      <Link href={`http://localhost:3000/rarity/${curr2}`}>
+                        <a>
+                          <Image
+                            src={`/images/NFT-med/${curr2}.jpg`}
+                            width="64"
+                            height="64"
+                            layout="fixed"
+                          />
+                        </a>
+                      </Link>
+                    );
+                  })}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+export default AttributeTable;
--- pages/attribute/index.js
+++ pages/attribute/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import attributeData from '../../data/attributeData.json';
 import numberData from '../../data/numberData.json';
 import rankingData from '../../data/rankingData.json';
+import AttributeTable from '../../components/AttributeTable';
 
 export const getStaticProps = async (context) => {
   // const res = await fetch(`http://localhost:3000/api/attributeData/`);
@@ -26,80 +27,17 @@ export const getStaticProps = async (context) => {
 function attribute({ attributeData, numberData, rankingData }) {
   return (
     <>
-      <section>
-        <h1>Attributes Type</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Types</th>
-              <th>Total</th>
-              <th>NFT</th>
-            </tr>
-          </thead>
-          <tbody>
-            {attributeData.map((curr) => {
-              return (
-                <tr>
-                  <td>{curr.id}</td>
-                  <td>{curr.total}</td>
-                  <td>
-                    {curr.accId.slice(0, 5).map((curr2) => {
-                      return (
-                        <Link href={`http://localhost:3000/rarity/${curr2}`}>
-                          <a>
-                            <Image
-                              src={`/images/NFT-med/${curr2}.jpg`}
-                              width="64"
-                              height="64"
-                            />
-                          </a>
-                        </Link>
-                      );
-                    })}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </section>
-      <section>
-        <h1>Attributes Amount</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Attributes Amount</th>
-              <th>Total</th>
-              <th>NFT</th>
-            </tr>
-          </thead>
-          <tbody>
-            {numberData.map((curr) => {
-              return (
-                <tr>
-                  <td>{curr.id.match(/\d/g)}</td>
-                  <td>{curr.total}</td>
-                  <td>
-                    {curr.accId.slice(0, 5).map((curr2) => {
-                      return (
-                        <Link href={`http://localhost:3000/rarity/${curr2}`}>
-                          <a>
-                            <Image
-                              src={`/images/NFT-med/${curr2}.jpg`}
-                              width="64"
-                              height="64"
-                            />
-                          </a>
-                        </Link>
-                      );
-                    })}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </section>
+      <AttributeTable
+        data={attributeData}
+        sectionHeader={'Attributes Type'}
+        tableHeader={['Types', 'Total', 'NFT']}
+      />
+      <AttributeTable
+        data={numberData}
+        sectionHeader={'Attributes Amount'}
+        tableHeader={['Attributes Amount', 'Total', 'NFT']}
+      />
+
       <section>
         <h1>Ranking</h1>
         <table>
