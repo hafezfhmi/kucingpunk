a310ce22f132bf0e5783636f42fba0c468c9fb86
hafezfhmi <hafezfahmi2000@gmail.com>
2021-09-16 05:10:18 +0800

    Added fade up transition using aos module to index page

--- components/BigLink.js
+++ components/BigLink.js
@@ -1,13 +1,23 @@
 import Button from './Button';
 import utilStyles from '../styles/Utils.module.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function Rarity({ url, desc1, desc2 }) {
+  useEffect(() => {
+    Aos.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <div>
       <Button url={url} buttonType="bigLink">
         {desc1}
       </Button>
-      <h3 className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}>
+      <h3
+        className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}
+        data-aos="fade-up"
+      >
         *Read: {desc2}
       </h3>
     </div>
--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,15 @@
 import styles from '../styles/Button.module.scss';
 import utilStyles from '../styles/Utils.module.scss';
 import Link from 'next/link';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function Button({ url, buttonType, children }) {
+  useEffect(() => {
+    Aos.init({ duration: 800, once: true });
+  }, []);
+
   if (buttonType == 'buy') {
     return (
       <a href={url} className={styles.buttonBuy}>
@@ -12,7 +19,12 @@ function Button({ url, buttonType, children }) {
   } else if (buttonType == 'bigLink') {
     return (
       <Link href={url}>
-        <a className={styles.bigLink + ' ' + utilStyles.headingL}>{children}</a>
+        <a
+          className={styles.bigLink + ' ' + utilStyles.headingL}
+          data-aos="fade-up"
+        >
+          {children}
+        </a>
       </Link>
     );
   }
