3ed0826dece812add3d5bc2f73a236fbb72d2143
hafezfhmi <hafezfahmi2000@gmail.com>
2021-09-16 16:00:49 +0800

    Changed modules from aos to react-reveal module for text appear on scroll effect

--- components/About.js
+++ components/About.js
@@ -1,41 +1,39 @@
 import styles from '../styles/About.module.scss';
 import utilStyles from '../styles/Utils.module.scss';
 import Image from 'next/image';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+
+// https://www.react-reveal.com/docs/
 
 function About() {
-  useEffect(() => {
-    Aos.init({ duration: 800, once: true });
-  }, []);
   return (
     <section className={styles.About + ' ' + utilStyles.margin2TopBot}>
-      <h2 className={utilStyles.headingL} data-aos="fade-up">
-        Meowhat?
-      </h2>
-      <h3
-        className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}
-        data-aos="fade-up"
-      >
-        *Read: What?
-      </h3>
+      <Fade bottom>
+        <h2 className={utilStyles.headingL}>Meowhat?</h2>
+        <h3 className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}>
+          *Read: What?
+        </h3>
+      </Fade>
+
       <div className={styles.container}>
         <div className={styles.subContainerLeft}>
-          <p data-aos="fade-up">KucingPunk is a collection of NFT.</p>
-          <hr className={styles.horizontalShort} data-aos="fade-up" />
-          <p data-aos="fade-up">Machinely generated with a total of 500 NFT.</p>
-          <hr className={styles.horizontalShort} data-aos="fade-up" />
-          <p data-aos="fade-up">
-            Each NFT is uniquely equipped with different traits and ranked by
-            their own rarity.
-          </p>
-          <hr className={styles.horizontalShort} data-aos="fade-up" />
-          <p data-aos="fade-up">
-            All KucingPunk will be minted on pentas.io and a total of 470 NFT
-            will be made available for sale.
-          </p>
+          <Fade bottom>
+            <p>KucingPunk is a collection of NFT.</p>
+            <hr className={styles.horizontalShort} />
+            <p>Machinely generated with a total of 500 NFT.</p>
+            <hr className={styles.horizontalShort} />
+            <p>
+              Each NFT is uniquely equipped with different traits and ranked by
+              their own rarity.
+            </p>
+            <hr className={styles.horizontalShort} />
+            <p>
+              All KucingPunk will be minted on pentas.io and a total of 470 NFT
+              will be made available for sale.
+            </p>
+          </Fade>
         </div>
+
         <div className={styles.subContainerRight}>
           <Image
             src="/images/Cat1.png"
--- components/BigLink.js
+++ components/BigLink.js
@@ -1,25 +1,18 @@
 import Button from './Button';
 import utilStyles from '../styles/Utils.module.scss';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
 
 function Rarity({ url, desc1, desc2 }) {
-  useEffect(() => {
-    Aos.init({ duration: 800, once: true });
-  }, []);
-
   return (
     <div>
-      <Button url={url} buttonType="bigLink">
-        {desc1}
-      </Button>
-      <h3
-        className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}
-        data-aos="fade-up"
-      >
-        *Read: {desc2}
-      </h3>
+      <Fade bottom>
+        <Button url={url} buttonType="bigLink">
+          {desc1}
+        </Button>
+        <h3 className={utilStyles.headingXS + ' ' + utilStyles.marginNeg2Top}>
+          *Read: {desc2}
+        </h3>
+      </Fade>
     </div>
   );
 }
